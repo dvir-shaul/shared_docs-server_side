d7286d0fb4838118f4d9353e03474784900dea18
zivhousler <zivhousler@gmail.com>
2022-11-17 14:08:02 +0200

    Added register functionalities for now.

--- src/main/java/docSharing/service/UserService.java
+++ src/main/java/docSharing/service/UserService.java
@@ -2,6 +2,7 @@ package docSharing.service;
 
 import docSharing.Entities.User;
 import docSharing.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLDataException;
@@ -9,12 +10,8 @@ import java.sql.SQLDataException;
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
+    @Autowired
+    private UserRepository userRepository;
 
     public User addUser(User user) throws SQLDataException {
         if(userRepository.findByEmail(user.getEmail())!=null){
