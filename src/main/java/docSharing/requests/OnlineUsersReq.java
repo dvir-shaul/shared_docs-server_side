d90294604907212065f9d90e9af7454198835070
Rachel <rachelibr050@gmail.com>
2022-11-27 12:12:19 +0200

    online users

---
+++ src/main/java/docSharing/requests/OnlineUsersReq.java
@@ -0,0 +1,13 @@
+package docSharing.requests;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class OnlineUsersReq {
+    private Long userId;
+    private Long docId;
+}
--- src/main/java/docSharing/service/DocumentService.java
+++ src/main/java/docSharing/service/DocumentService.java
@@ -259,7 +259,7 @@ public class DocumentService implements ServiceInterface {
         userDocumentRepository.deleteDocument(documentRepository.findById(docId).get());
         documentRepository.deleteById(docId);
     }
-    public List<User> getOnlineUsers(Document doc){
+    public Set<User> getOnlineUsers(Document doc){
         return doc.getOnlineUsers();
     }
 }
