b98ad752c2f506ef37ae44b923bed93cba09ee1c
Rachel <rachelibr050@gmail.com>
2022-11-28 12:04:35 +0200

    get path by doc id

--- src/main/java/docSharing/entity/Document.java
+++ src/main/java/docSharing/entity/Document.java
@@ -73,12 +73,12 @@ public class Document extends GeneralItem {
         onlineUsers.removeIf(u -> u.getId().equals(user.getId()));
     }
 
-    @Override
-    public String toString() {
-        return "Document{" +
-                "isPrivate=" + isPrivate +
-                ", content='" + content + '\'' +
-                ", onlineUsers=" + onlineUsers +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return super.toString()+
+//                "isPrivate=" + isPrivate +
+//                ", content='" + content + '\'' +
+//                ", onlineUsers=" + onlineUsers +
+//                '}';
+//    }
 }
---
+++ src/main/java/docSharing/response/PathItem.java
@@ -0,0 +1,15 @@
+package docSharing.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PathItem {
+    private Long itemId;
+    private String itemName;
+}
