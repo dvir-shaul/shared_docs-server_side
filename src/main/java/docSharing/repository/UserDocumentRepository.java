2db5ce80e70f77a0d2a7c99c209683b7147710ca
Rachel <rachelibr050@gmail.com>
2022-12-04 21:05:21 +0200

    change updatePermission

--- src/main/java/docSharing/repository/UserDocumentRepository.java
+++ src/main/java/docSharing/repository/UserDocumentRepository.java
@@ -19,11 +19,12 @@ public interface UserDocumentRepository extends JpaRepository<UserDocument, Long
     @Query("SELECT u FROM UserDocument u WHERE u.user = ?1")
     List<UserDocument> findByUser(User user);
 
+
     @Query("SELECT u FROM UserDocument u WHERE u.document=?1")
     List<UserDocument> findAllUsersInDocument(Document document);
 
     @Transactional
-    @Modifying
+    @Modifying(clearAutomatically = true)
     @Query("UPDATE UserDocument urd SET urd.permission = ?1 WHERE (urd.document = ?2 and urd.user = ?3)")
     int updatePermission(Permission permission, Document document, User user);
 
