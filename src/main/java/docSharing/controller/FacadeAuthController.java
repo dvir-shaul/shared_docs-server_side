d56600829fc72b82068ed686131dc400dc4a5a45
Dvir Shaul <dvir.shaul@gmail.com>
2022-12-05 11:52:03 +0200

    organize the code and add loggers + documentation

--- src/main/java/docSharing/controller/FacadeAuthController.java
+++ src/main/java/docSharing/controller/FacadeAuthController.java
@@ -46,7 +46,7 @@ public class FacadeAuthController {
                     .statusCode(400)
                     .build();
         }
-        Validations.validate(Regex.NAME.getRegex(), name);
+//        Validations.validate(Regex.NAME.getRegex(), name);
         Validations.validate(Regex.EMAIL.getRegex(), email);
         Validations.validate(Regex.PASSWORD.getRegex(), password);
         User emailUser = authService.register(email, password, name);
