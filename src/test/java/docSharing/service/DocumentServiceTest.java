977cd4f14694d26fb7896aaa9eac1b6273324a0c
Rachel <rachelibr050@gmail.com>
2022-12-06 09:21:24 +0200

    DocumentServiceTest names finished

--- src/test/java/docSharing/service/DocumentServiceTest.java
+++ src/test/java/docSharing/service/DocumentServiceTest.java
@@ -7,6 +7,7 @@ import docSharing.requests.Method;
 import docSharing.utils.logAction;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -66,37 +67,44 @@ class DocumentServiceTest {
     }
 
     @Test
+    @DisplayName("Make sure a list of active user contains the user when calling getActiveUsers with valid parameters and ADD method")
     public void getActiveUsers_validParametersMethodADD_returnUsers() throws AccountNotFoundException {
         given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
         assertTrue(documentService.getActiveUsers(user.getId(), document.getId(), Method.ADD).contains(user));
     }
 
     @Test
+    @DisplayName("Make sure a list of active user does not contain the user when calling getActiveUsers with valid parameters and REMOVE method")
     public void getActiveUsers_validParametersMethodREMOVE_returnUsers() throws AccountNotFoundException {
         given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
         assertFalse(documentService.getActiveUsers(user.getId(), document.getId(), Method.REMOVE).contains(user));
     }
     @Test
+    @DisplayName("Make sure a list of active user is returned when calling getActiveUsers with valid parameters and GET method")
     public void getActiveUsers_validParametersMethodGET_returnUsers() throws AccountNotFoundException {
         given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
         assertNotNull(documentService.getActiveUsers(user.getId(), document.getId(), Method.GET));
     }
     @Test
+    @DisplayName("Make sure an exception is thrown when calling getActiveUsers with invalid parameters")
     public void getActiveUsers_invalidParameters_throwsAccountNotFound() {
         given(userRepository.findById(user.getId())).willReturn(Optional.empty());
         assertThrows(AccountNotFoundException.class,()-> documentService.getActiveUsers(user.getId(), document.getId(), Method.GET));
     }
     @Test
+    @DisplayName("Make sure path of document is returned when calling getPath with valid parameters")
     public void getPath_validParameters_returnsPath() throws FileNotFoundException {
         given(documentRepository.findById(document.getId())).willReturn(Optional.of(document));
         assertNotNull(documentService.getPath(document.getId()));
     }
     @Test
+    @DisplayName("Make sure an exception is thrown when calling getPath with invalid parameters")
     public void getPath_documentNotExist_throwsFileNotFound() throws FileNotFoundException {
         given(documentRepository.findById(document.getId())).willReturn(Optional.empty());
         assertThrows(FileNotFoundException.class,()->documentService.getPath(document.getId()));
     }
     @Test
+    @DisplayName("Make sure permission is returned when calling getUserPermissionInDocument with valid parameters")
     public void getUserPermissionInDocument_validParameters_returnPermission() throws FileNotFoundException, AccountNotFoundException {
         given(documentRepository.findById(document.getId())).willReturn(Optional.of(document));
         given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
@@ -104,6 +112,7 @@ class DocumentServiceTest {
         assertNotNull(documentService.getUserPermissionInDocument(user.getId(), document.getId()));
     }
     @Test
+    @DisplayName("Make sure permission UNAUTHORIZED is returned when calling getUserPermissionInDocument when user does not have permission in a document")
     public void getUserPermissionInDocument_userInDocumentNotExists_returnUNAUTHORIZED() throws FileNotFoundException, AccountNotFoundException {
         given(documentRepository.findById(document.getId())).willReturn(Optional.of(document));
         given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
@@ -111,25 +120,30 @@ class DocumentServiceTest {
         assertEquals(Permission.UNAUTHORIZED,documentService.getUserPermissionInDocument(user.getId(), document.getId()));
     }
     @Test
+    @DisplayName("Make sure an exception is thrown when calling getUserPermissionInDocument when user does not exist")
     public void getUserPermissionInDocument_userNotExists_throwAccountNotFound() throws FileNotFoundException {
         given(documentRepository.findById(document.getId())).willReturn(Optional.of(document));
         given(userRepository.findById(user.getId())).willReturn(Optional.empty());
         assertThrows(AccountNotFoundException.class, ()->documentService.getUserPermissionInDocument(user.getId(), document.getId()));
     }
     @Test
+    @DisplayName("Make sure an exception is thrown when calling getUserPermissionInDocument when document does not exist")
     public void getUserPermissionInDocument_documentNotExists_throwFileNotFound() throws FileNotFoundException {
         given(documentRepository.findById(document.getId())).willReturn(Optional.empty());
         assertThrows(FileNotFoundException.class, ()->documentService.getUserPermissionInDocument(user.getId(), document.getId()));
     }
     @Test
+    @DisplayName("Make sure user-document is being saved when calling saveUserInDocument with valid parameters")
     public void saveUserInDocument_validParameters_returnUserInDocument(){
         assertNotNull(documentService.saveUserInDocument(userDocument));
     }
     @Test
-    public void saveUserInDocument_null_returnUserInDocument(){
+    @DisplayName("Make sure an exception is thrown when calling saveUserInDocument with null")
+    public void saveUserInDocument_null_throwNullPointerException(){
        assertThrows(NullPointerException.class,()->documentService.saveUserInDocument(null));
     }
     @Test
+    @DisplayName("Make sure content is updated when calling updateContent with valid log")
     public void updateContent_validLog_updateContent(){
         given(documentRepository.findById(log.getDocument().getId())).willReturn(Optional.of(document));
     }
