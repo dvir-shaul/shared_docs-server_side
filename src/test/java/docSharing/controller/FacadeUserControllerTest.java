e20406b80c3cf05ed9e794a42f2528c20aadcabb
zivhousler <zivhousler@gmail.com>
2022-12-06 10:03:42 +0200

    finished usercontrollertest facade tests names

--- src/test/java/docSharing/controller/FacadeUserControllerTest.java
+++ src/test/java/docSharing/controller/FacadeUserControllerTest.java
@@ -11,6 +11,7 @@ import docSharing.service.DocumentService;
 import docSharing.service.UserService;
 import docSharing.utils.EmailUtil;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -51,6 +52,7 @@ public class FacadeUserControllerTest {
 
 
     @BeforeEach
+    @DisplayName("Make sure all the correct parameters are refreshed after each operation")
     void setUp() {
         goodUser = User.createUser("asaf396@gmai.com", "dvir1234", "dvir");
         goodUser.setId(1L);
@@ -59,11 +61,13 @@ public class FacadeUserControllerTest {
     }
 
     @Test
+    @DisplayName("Trying to give a permission, without a permission parameter, will throw an exception")
     void givePermission_LackPermission_throwsNullArgumentException() {
         assertEquals(400, facadeUserController.givePermission(1L, 1L, null).getStatusCode(), "Trying to change permission to user with a null permission did not throw errorCode 400");
     }
 
     @Test
+    @DisplayName("Given all the correct parameters, when trying to give permission to a user, successfully change his permission")
     void givePermision_successful() throws AccountNotFoundException {
         UserService us = mock(UserService.class);
         given(documentService.getAllUsersInDocument(1L, 1L, Method.GET)).willReturn(usersInDoc);
@@ -71,30 +75,35 @@ public class FacadeUserControllerTest {
     }
 
     @Test
-    void givePermission_noUserId_successful() {
+    @DisplayName("Trying to remove any permissions to a user using his ID, will lead to a successful remove from the database")
+    void givePermission_removeFromPermissions_successful() {
         assertEquals(200, facadeUserController.givePermission(1L, 1L, Permission.UNAUTHORIZED).getStatusCode(), "change permission to unregistered user did not return responseCode 400");
     }
 
     @Test
+    @DisplayName("Given a wrong document ID, when trying to remove a permission to a user, lead to an exception to be thrown")
     void givePermission_wrongDocumentId_BAD_REQUEST() throws AccountNotFoundException {
         doThrow(IllegalArgumentException.class).when(documentService).getAllUsersInDocument(1L, 1L, Method.GET);
         assertEquals(400, facadeUserController.givePermission(1L, 1L, Permission.UNAUTHORIZED).getStatusCode(), "change permission to non-existing document did not return responseCode 400");
     }
 
     @Test
+    @DisplayName("Give permission to a list of users will lead to a successful operation if all the parameters are correct")
     void givePermissionToAll_EmptyList_successful() {
         List<String> emails = new ArrayList<>();
         assertEquals(200, facadeUserController.givePermissionToAll(emails, 1L).getStatusCode());
     }
 
     @Test
+    @DisplayName("Given wrong emails to grant permission will lead to a bad request response")
     void givePermissionToAll_wrongEmailsOrUnregistered_BAD_REQUEST() {
         List<String> emails = new ArrayList<>();
         assertEquals(200, facadeUserController.givePermissionToAll(emails, 1L).getStatusCode());
     }
 
     @Test
-    void givePermissionToAll_unauthorizedUser_BAD_REQUEST() throws AccountNotFoundException, FileNotFoundException {
+    @DisplayName("When an unauthorized user tries to change permissions to users, a ")
+    void givePermissionToAll_inviteUnauthorizedUser_OK() throws AccountNotFoundException, FileNotFoundException {
         List<String> emails = new ArrayList<>();
         emails.add("ziv@gmail.com");
         given(userService.findByEmail("ziv@gmail.com")).willReturn(goodUser);
@@ -104,41 +113,48 @@ public class FacadeUserControllerTest {
     }
 
     @Test
+    @DisplayName("When a registered user asks for his documents, given all correct in formation, successfully return all documents to him")
     void getDocumentsForUser_whenUserExists_successful() {
         assertEquals(200, facadeUserController.getDocuments(1L).getStatusCode());
     }
 
 
     @Test
+    @DisplayName("If a user doesn't exist in the database, don't return his files and throw an exception / bad request response")
     void getDocumentsForUser_whenUserDoesntExist_BAD_REQUEST() throws AccountNotFoundException {
         given(userService.documentsOfUser(1L)).willThrow(AccountNotFoundException.class);
         assertEquals(400, facadeUserController.getDocuments(1L).getStatusCode());
     }
 
     @Test
+    @DisplayName("Once trying to fetch a user information, given an existing user, successfully return his data")
     void getUser_whenUserExists_successful() {
         assertEquals(200, facadeUserController.getUser(1L).getStatusCode());
     }
 
     @Test
+    @DisplayName("Once trying to fetch a user information, given a non-existing user, fail to return his data")
     void getUser_whenUserDoesntExist_BAD_REQUEST() throws AccountNotFoundException {
         given(userService.getUser(1L)).willThrow(AccountNotFoundException.class);
         assertEquals(400, facadeUserController.getUser(1L).getStatusCode());
     }
 
     @Test
+    @DisplayName("When trying to fetch a user permission for a specific document, given all the correct information, return a good response")
     void getUserPermissionForDocument_whenUserExists_succesful() throws AccountNotFoundException {
         given(userService.findById(goodUser.getId())).willReturn(goodUser);
         assertEquals(200, facadeUserController.getUserPermissionForSpecificDocument(1L, 1L).getStatusCode());
     }
 
     @Test
+    @DisplayName("When trying to get a permission of a user for a specific document, if he doesn't exist, return a bad request")
     void getUserPermissionForDocument_whenUserDoesntExist_fail() throws AccountNotFoundException {
         given(userService.findById(1L)).willThrow(AccountNotFoundException.class);
         assertEquals(400, facadeUserController.getUserPermissionForSpecificDocument(1L, 1L).getStatusCode());
     }
 
     @Test
+    @DisplayName("When trying to get a permission of a user for a specific document, and the document doesn't exist, return a bad request")
     void getUserPermissionForDocument_whenDocumentDoesntExist_fail() throws FileNotFoundException, AccountNotFoundException {
         given(documentService.getUserPermissionInDocument(1L, 1L)).willThrow(FileNotFoundException.class);
         assertEquals(400, facadeUserController.getUserPermissionForSpecificDocument(1L, 1L).getStatusCode());
